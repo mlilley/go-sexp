e0e7bd63c5ad10caa0704312cdd0722eb8f4dd63
Mike <michael.lilley@gmail.com>
2024-04-07 16:58:56 -0700

    typed structs

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ import (
 	"unicode"
 )
 
+type Sexpr struct {
+	params []SexprParam
+}
+
+type SexprParam interface {
+	String() string
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("expect filename as argument")
@@ -24,53 +32,149 @@ func main() {
 	}
 	defer f.Close()
 
-	sexp, err := parse(bufio.NewReader(f), false)
+	sexpr, err := ParseSexpr(bufio.NewReader(f), false)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(sexp.String())
+	fmt.Println(sexpr.String())
 }
 
-type SExp struct {
-	params []SParam
+// ---
+
+func (s *Sexpr) GetParam(index int) SexprParam {
+	return s.params[index]
 }
 
-type SParam interface{} // (string, int, float64, or SExp)
+func (s *Sexpr) AddParam(param SexprParam) {
+	s.params = append(s.params, param)
+}
 
-func (sexp *SExp) String() string {
+func (s *Sexpr) UpdateParam(index int, param SexprParam) {
+	s.params[index] = param
+}
+
+func (s *Sexpr) Size() int {
+	return len(s.params)
+}
+
+func (s Sexpr) String() string {
 	var sb strings.Builder
-	stringSexp(sexp, &sb, 0)
+	s.string_(&sb, 0)
 	return sb.String()
 }
 
-func stringSexp(sexp *SExp, acc *strings.Builder, level int) {
+func (s *Sexpr) string_(acc *strings.Builder, level int) {
 	indent := strings.Repeat("  ", level)
 	acc.WriteString(indent)
 	acc.WriteString("(\n")
-	for _, param := range sexp.params {
-		switch t := param.(type) {
-		case string:
+	for _, param := range s.params {
+		if sexpr, ok := param.(*Sexpr); ok {
+			sexpr.string_(acc, level+1)
+		} else {
 			acc.WriteString(indent)
-			acc.WriteString(t)
+			acc.WriteString("  ")
+			acc.WriteString(param.String())
 			acc.WriteString("\n")
-		case int:
-			acc.WriteString(indent)
-			acc.WriteString(strconv.Itoa(t))
-			acc.WriteString("\n")
-		case float64:
-			acc.WriteString(indent)
-			acc.WriteString(strconv.FormatFloat(t, 'f', -1, 64))
-			acc.WriteString("\n")
-		case *SExp:
-			stringSexp(t, acc, level+1)
 		}
 	}
 	acc.WriteString(indent)
 	acc.WriteString(")\n")
 }
 
-func parse(reader *bufio.Reader, parseNumerics bool) (*SExp, error) {
+// ---
+
+type StringParam struct {
+	value string
+}
+
+func (p *StringParam) String() string {
+	return p.value
+}
+func (p *StringParam) SetValue(value string) {
+	p.value = value // todo: check valid unquoted string param value
+}
+func NewStringParam(value string) *StringParam {
+	return &StringParam{value} // todo: check valid unquoted string param value
+}
+
+// ---
+
+type QuotedStringParam struct {
+	value string
+}
+
+func (p *QuotedStringParam) String() string {
+	return `"` + p.value + `"`
+}
+func (p *QuotedStringParam) SetValue(unquotedValue string) {
+	p.value = unquotedValue
+}
+func NewQuotedStringParam(unquotedValue string) *QuotedStringParam {
+	return &QuotedStringParam{unquotedValue}
+}
+
+// ---
+
+type IntParam struct {
+	value int
+}
+
+func (p *IntParam) String() string {
+	return strconv.Itoa(p.value)
+}
+func (p *IntParam) GetValue() int {
+	return p.value
+}
+func (p *IntParam) SetValue(value int) {
+	p.value = value
+}
+func NewIntParam(value int) *IntParam {
+	return &IntParam{value}
+}
+func NewIntParamFromInput(value string) (*IntParam, error) {
+	v, err := strconv.Atoi(value)
+	if err != nil {
+		return nil, err
+	}
+	return &IntParam{v}, nil
+}
+
+// ---
+
+type FloatParam struct {
+	value      float64
+	changed    bool
+	inputValue string // to prevent reserializing float values if unchanged
+}
+
+func (p *FloatParam) String() string {
+	if p.changed {
+		return strconv.FormatFloat(p.value, 'f', -1, 64)
+	}
+	return p.inputValue
+}
+func (p *FloatParam) GetValue() float64 {
+	return p.value
+}
+func (p *FloatParam) Setvalue(value float64) {
+	p.changed = true
+	p.value = value
+}
+func (p *FloatParam) NewFloatParam(value float64) *FloatParam {
+	return &FloatParam{value, true, ""}
+}
+func NewFloatParamFromInput(value string) (*FloatParam, error) {
+	v, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return nil, err
+	}
+	return &FloatParam{v, false, value}, nil
+}
+
+// ---
+
+func ParseSexpr(reader *bufio.Reader, parseNumerics bool) (*Sexpr, error) {
 	token, err := nextToken(reader, parseNumerics)
 	if err != nil {
 		// an EOF terminates an empty input file
@@ -83,26 +187,26 @@ func parse(reader *bufio.Reader, parseNumerics bool) (*SExp, error) {
 	switch t := token.(type) {
 	case rune:
 		if t == '(' {
-			sexp, err := parseSexp(reader, parseNumerics)
+			sexpr, err := parseSexpr(reader, parseNumerics)
 			if err != nil {
 				return nil, err
 			}
-			return sexp, nil
+			return sexpr, nil
 		}
 	}
 
 	return nil, errors.New("unexpected token")
 }
 
-func parseSexp(reader *bufio.Reader, parseNumerics bool) (*SExp, error) {
-	sexp := SExp{}
+func parseSexpr(reader *bufio.Reader, parseNumerics bool) (*Sexpr, error) {
+	sexpr := &Sexpr{}
 
 	for {
 		token, err := nextToken(reader, parseNumerics)
 		if err != nil {
 			if err == io.EOF {
-				// an EOF illegally terminates an sexp
-				return nil, errors.New("unexpected EOF reading sexp")
+				// an EOF illegally terminates an sexpr
+				return nil, errors.New("unexpected EOF reading sexpr")
 			}
 			return nil, err
 		}
@@ -111,33 +215,31 @@ func parseSexp(reader *bufio.Reader, parseNumerics bool) (*SExp, error) {
 		case rune:
 			// proper termination of sexp
 			if t == ')' {
-				return &sexp, nil
+				return sexpr, nil
 			}
-			// param is a sexp, recurse into it
+			// param is a sexpr, recurse into it
 			if t == '(' {
-				sexpParam, err := parseSexp(reader, parseNumerics)
+				sexpr2, err := parseSexpr(reader, parseNumerics)
 				if err != nil {
 					if err == io.EOF {
 						return nil, errors.New("unexpected EOF reading sexp")
 					}
 					return nil, err
 				}
-				sexp.params = append(sexp.params, sexpParam)
+				sexpr.AddParam(sexpr2)
 				continue
 			}
 			// unexpected rune
 			return nil, errors.New("unexpected token (rune)")
 
-		default:
-			// token is string, int, or float64
-			sexp.params = append(sexp.params, token)
-			continue
+		case SexprParam:
+			sexpr.AddParam(t)
 		}
 	}
 }
 
-// returns rune, string, int, or float
-func nextToken(reader *bufio.Reader, parseNumerics bool) (interface{}, error) {
+// returns rune or SexprParam
+func nextToken(reader *bufio.Reader, parseNumerics bool) (any, error) {
 	inQuoted := false
 	inUnquoted := false
 	var token string
@@ -161,7 +263,7 @@ func nextToken(reader *bufio.Reader, parseNumerics bool) (interface{}, error) {
 		if inQuoted {
 			// only a quote terminates a quoted token
 			if r == '"' {
-				return token, nil
+				return NewQuotedStringParam(token), nil
 			}
 
 			// append rune to quoted token
@@ -171,41 +273,43 @@ func nextToken(reader *bufio.Reader, parseNumerics bool) (interface{}, error) {
 
 		if inUnquoted {
 			if r == '(' {
-				// illegal sexp start
-				return nil, errors.New("illegal sexp start")
+				// illegal sexpr start
+				return nil, errors.New("illegal sexpr start")
 			}
 			if r == '"' {
 				// illegal quoted start
 				return nil, errors.New("illegal quoted start")
 			}
 			if r == ')' {
-				// push back the ), end and return the unquoted token
+				// push back the ), end token, and return it
 				err = reader.UnreadRune()
 				if err != nil {
 					return nil, err
 				}
 
 				if parseNumerics {
-					if v, err := strconv.Atoi(token); err == nil {
+					if v, err := NewIntParamFromInput(token); err == nil {
 						return v, nil
 					}
-					if v, err := strconv.ParseFloat(token, 64); err == nil {
+					if v, err := NewFloatParamFromInput(token); err == nil {
 						return v, nil
 					}
 				}
-				return token, nil
+
+				return NewStringParam(token), nil
 			}
 			if unicode.IsSpace(r) {
-				// end and return the unquoted token
+				// end token, and return it
 				if parseNumerics {
-					if v, err := strconv.Atoi(token); err == nil {
+					if v, err := NewIntParamFromInput(token); err == nil {
 						return v, nil
 					}
-					if v, err := strconv.ParseFloat(token, 64); err == nil {
+					if v, err := NewFloatParamFromInput(token); err == nil {
 						return v, nil
 					}
 				}
-				return token, nil
+
+				return NewStringParam(token), nil
 			}
 
 			// add the non-(, non-), non-", non-ws rune to the unquoted token
